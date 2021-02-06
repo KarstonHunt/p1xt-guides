e7341f8b7e284fcf7863eedaa2769398f1515587
Jason Barr <jason@jasonsbarr.com>
2021-02-06 16:22:24 -0600

    Do substrings function

--- Tier 0/3 - Software Engineering Foundations/rspec_exercise_2/lib/part_2.rb
+++ Tier 0/3 - Software Engineering Foundations/rspec_exercise_2/lib/part_2.rb
@@ -3,3 +3,17 @@ def palindrome?(str)
   return false if str[0] != str[-1]
   return palindrome?(str[1..-2])
 end
+
+def substrings(str)
+  substrings = []
+  str.each_char.with_index do |char1, i|
+    substr = ""
+    str.each_char.with_index do |char2, j|
+      if j >= i
+        substr += char2
+        substrings << substr
+      end
+    end
+  end
+  substrings
+end
