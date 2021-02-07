970e3a5e33717052a74999b642ff891fc5ed7ad2
Jason Barr <jason@jasonsbarr.com>
2021-02-07 17:01:44 -0600

    Do greater_proc_value function

--- Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
+++ Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
@@ -8,3 +8,11 @@ def word_changer(sentence, &prc)
   new_words = words.map { |word| prc.call(word) }
   new_words.join(" ")
 end
+
+def greater_proc_value(num, prc1, prc2)
+  if prc1.call(num) > prc2.call(num)
+    return prc1.call(num)
+  else
+    return prc2.call(num)
+  end
+end
