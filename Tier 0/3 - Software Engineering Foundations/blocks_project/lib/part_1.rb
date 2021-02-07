02d155bbf34709fbea2a85c03dd32f3fe9067e8c
Jason Barr <jason@jasonsbarr.com>
2021-02-07 12:19:04 -0600

    Do aba_translate function

--- Tier 0/3 - Software Engineering Foundations/blocks_project/lib/part_1.rb
+++ Tier 0/3 - Software Engineering Foundations/blocks_project/lib/part_1.rb
@@ -9,3 +9,15 @@ end
 def count_positive_subarrays(array)
   array.count { |subarr| subarr.sum > 0 }
 end
+
+def aba_translate(word)
+  vowels = "aeiou"
+  translated = ""
+  word.each_char do |char|
+    translated += char
+    if vowels.include?(char.downcase)
+      translated += "b" + char
+    end
+  end
+  translated
+end
