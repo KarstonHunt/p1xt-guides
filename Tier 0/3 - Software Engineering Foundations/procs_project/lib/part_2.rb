4a8b14eb3a2b4d8b3415a9c4f3ab218fd297e8bf
Jason Barr <jason@jasonsbarr.com>
2021-02-07 16:53:47 -0600

    Do word_changer function

--- Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
+++ Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
@@ -2,3 +2,8 @@ def reverser(str, &prc)
   reversed = str.reverse
   prc.call(reversed)
 end
+
+def word_changer(sentence, &prc)
+  words = sentence.split(" ")
+  new_words = words.map { |word|  }
+end
