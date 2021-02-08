a9c261b606dfa44c189a5c8cedb747f5d56c4d11
Jason Barr <jason@jasonsbarr.com>
2021-02-08 16:22:30 -0600

    Do and_selector function

--- Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
+++ Tier 0/3 - Software Engineering Foundations/procs_project/lib/part_2.rb
@@ -16,3 +16,11 @@ def greater_proc_value(num, prc1, prc2)
     return prc2.call(num)
   end
 end
+
+def and_selector(arr, prc1, prc2)
+  new_arr = []
+  arr.each do |el|
+    new_arr << el if prc1.call(el) && prc2.call(el)
+  end
+  new_arr
+end
