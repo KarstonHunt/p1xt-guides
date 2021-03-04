c8013f277ab48680ee2af0a7be2f6d743b292f05
Jason Barr <jason@jasonsbarr.com>
2021-03-04 16:06:04 -0600

    Finish mastermind project

--- Tier 0/3 - Software Engineering Foundations/mastermind_project/lib/code.rb
+++ Tier 0/3 - Software Engineering Foundations/mastermind_project/lib/code.rb
@@ -1,4 +1,6 @@
 class Code
+  attr_reader :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -6,4 +8,57 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.key?(char.upcase) }
+  end
+
+  def initialize(chars)
+    if Code.valid_pegs?(chars)
+      @pegs = chars.map(&:upcase)
+    else
+      raise "Invalid peg(s)"
+    end
+  end
+
+  def self.random(length)
+    pegs = []
+    length.times do |n|
+      pegs << POSSIBLE_PEGS.keys.sample
+    end
+    Code.new(pegs)
+  end
+
+  def self.from_string(str)
+    Code.new(str.split(""))
+  end
+
+  def [](i)
+    @pegs[i]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(guess)
+    matches = 0
+    (0...guess.length).each do |i|
+      matches += 1 if guess[i] == self[i]
+    end
+    matches
+  end
+
+  def num_near_matches(guess)
+    count = 0
+    (0...guess.length).each do |i|
+      if guess[i] != self[i] && self.pegs.include?(guess[i])
+        count += 1
+      end
+    end
+    count
+  end
+
+  def ==(guess)
+    self.pegs == guess.pegs
+  end
 end
--- Tier 0/3 - Software Engineering Foundations/mastermind_project/lib/mastermind.rb
+++ Tier 0/3 - Software Engineering Foundations/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    puts "Exact matches: #{@secret_code.num_exact_matches(code)}"
+    puts "Near matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    guess = gets.chomp
+    guess = Code.from_string(guess)
+    self.print_matches(guess)
+    return guess == @secret_code
+  end
 end
